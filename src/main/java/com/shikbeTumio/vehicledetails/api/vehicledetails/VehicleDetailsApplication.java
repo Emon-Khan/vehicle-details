fec6f1bf77db53382df39a740c72eb199f893f8a
Emon-Khan <rkemon94@gmail.com>
2024-01-03 12:48:10 +0600

    Some changes have been done

--- src/main/java/com/shikbeTumio/vehicledetails/api/vehicledetails/VehicleDetailsApplication.java
+++ src/main/java/com/shikbeTumio/vehicledetails/api/vehicledetails/VehicleDetailsApplication.java
@@ -3,8 +3,10 @@ package com.shikbeTumio.vehicledetails.api.vehicledetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VehicleDetailsApplication {
 
 	public static void main(String[] args) {
